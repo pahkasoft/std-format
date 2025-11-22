a08bfbcfbf88b127ad19c8dfa03f39a6692b86f5
Stefan B. <pahkasoft@outlook.com>
2025-11-22 13:11:54 +0200

    refactor: test script update.

--- extras/test-scripts/node-cjs.js
+++ extras/test-scripts/node-cjs.js
@@ -1,9 +1,12 @@
-const BraceFormat = require("@tspro/brace-format");
-const StdFormat = require("@sbrockma/std-format");
+const Format1 = require("std-format");
+const Format2 = require("@sbrockma/std-format");
+const Format3 = require("@tspro/brace-format");
 const testFormatLib = require("@tspro/test-format-lib");
 
 console.log("---- Node CJS Test ----");
-testFormatLib(BraceFormat, console.log);
+testFormatLib(Format1, console.log);
 console.log("");
-testFormatLib(StdFormat, console.log);
+testFormatLib(Format2, console.log);
+console.log("");
+testFormatLib(Format3, console.log);
 console.log("");
--- extras/test-scripts/node-esm.mjs
+++ extras/test-scripts/node-esm.mjs
@@ -1,9 +1,12 @@
-import * as BraceFormat from "@tspro/brace-format";
-import * as StdFormat from "@sbrockma/std-format";
+import * as Format1 from "std-format";
+import * as Format2 from "@sbrockma/std-format";
+import * as Format3 from "@tspro/brace-format";
 import testFormatLib from "@tspro/test-format-lib";
 
 console.log("---- Node ESM Test ----");
-testFormatLib(BraceFormat, console.log);
+testFormatLib(Format1, console.log);
 console.log("");
-testFormatLib(StdFormat, console.log);
+testFormatLib(Format2, console.log);
+console.log("");
+testFormatLib(Format3, console.log);
 console.log("");
